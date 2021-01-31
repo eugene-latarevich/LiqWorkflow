1d5a3bd240199e415a1fe47eee9c43948002e4b3
Евгений Латаревич <eugene.latarevich@gmail.com>
2021-01-31 17:32:34 +0300

    ServiceCollectionExtensions

--- src/LiqWorkflow/Common/Extensions/ServiceCollectionExtensions.cs
+++ src/LiqWorkflow/Common/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using LiqWorkflow.Abstractions.Events;
+﻿using LiqWorkflow.Abstractions.Builders;
+using LiqWorkflow.Abstractions.Events;
 using LiqWorkflow.Abstractions.Factories;
+using LiqWorkflow.Builders;
 using LiqWorkflow.Events.Brokers;
 using LiqWorkflow.Factories;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ namespace LiqWorkflow.Common.Extensions
     {
         public static IServiceCollection AddLiqWorkflow(this IServiceCollection services)
         {
+            services.AddTransient<IWorkflowBuilder, WorkflowBuilder>();
             services.AddTransient<IWorkflowBranchFactory, WorkflowBranchFactory>();
             services.AddTransient<IWorkflowMessageEventBroker, WorkflowMessageEventBroker>();
 
